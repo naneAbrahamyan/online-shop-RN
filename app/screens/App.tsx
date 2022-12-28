27ccf800cc88e7956e788f8a80ac0cea8a585be9
naneAbrahamyan <117164945+naneAbrahamyan@users.noreply.github.com>
2022-12-28 15:19:13 +0200

    Add files via upload

---
+++ app/screens/App.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import RoundIcon from './app/components/RoundIcon';
+import colors from './app/configs/colors';
+import LoginScreen from './app/screens/LoginScreen';
+import Screen from './app/components/Screen';
+import ShopScreen from './app/screens/ShopScreen';
+import ItemScreen from './app/screens/ItemScreen';
+import CartScreen from './app/screens/Cart';
+import ProductsNavigator from './app/navigation/ProductsNavigator';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+      // <LoginScreen />
+      // <ShopScreen />
+      // <ItemScreen />
+      // <CartScreen />
+      <NavigationContainer>
+
+      <Tab.Navigator screenOptions={{
+        headerShown: false, 
+      }}>
+        <Tab.Screen name="Products" component={ProductsNavigator} options = {{  tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons name = "food" size = {size} color = {color}/>
+        )}}/>
+        <Tab.Screen name="Cart" component={CartScreen} options = {{  tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons name = "cart" size = {size} color = {color}/>
+        )}}/>
+        <Tab.Screen name="Settings" component={CartScreen} options = {{  tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons name = "account-settings" size = {size} color = {color}/>
+      )}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems:'center',
+    backgroundColor:colors.purple,
+    flex:1,
+    justifyContent: 'center'
+  },
+});
